9f2d040001676e30d5d28cc44d3356999bef3e4e
Jacques Duflos <j.duflos@ymail.com>
2023-07-22 23:58:40 -0500

    first commit

---
+++ traduction.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from gimpfu import *
+from translate import Translator
+import re
+import HTMLParser
+
+
+class HTMLDecodeParser(HTMLParser.HTMLParser):
+    def __init__(self):
+        HTMLParser.HTMLParser.__init__(self)
+        self.result = ""
+
+    def handle_data(self, data):
+        self.result += data
+        
+        
+def translate_text_layers(image, drawable):
+    image.undo_group_start()
+    
+    translator = Translator(to_lang="fr", from_lang="es")
+    print("------------start--------------")
+    pdb.gimp_message_set_handler(MESSAGE_BOX)
+    
+    # Recuperation du groupe de calques selectionne
+    selected_layer = pdb.gimp_image_get_active_layer(image)
+    
+    # Récupération du groupe de calques "text-fr" s'il existe
+    group_text = None
+    for layer in image.layers:
+        if pdb.gimp_item_is_group(layer) and pdb.gimp_item_get_name(layer) == "text-fr":
+            group_text = layer
+            print ("------------groupe preexistant-------------")
+            break
+
+    if group_text is None:
+        # Créer un groupe de calques nommé "text-fr" s'il n'existe pas
+        group_text = pdb.gimp_layer_group_new(image)
+        pdb.gimp_item_set_name(group_text, "text-fr")
+        pdb.gimp_image_insert_layer(image, group_text, None, 0)
+        print("----------------groupe créé-----------------")
+    if pdb.gimp_item_is_group(selected_layer):
+        print("----------layer is group-------------")
+        layers = selected_layer.layers
+        print (layers)
+    elif pdb.gimp_item_is_text_layer(selected_layer):
+        print("----------layer is text-------------")
+        layers=[selected_layer]
+        print (layers)
+    else:
+        print("--------Veuillez sélectionner un groupe de calques ou un calque de texte.----")
+        gimp.message("Veuillez sélectionner un groupe de calques ou un calque de texte.")
+        image.undo_group_end()
+        #return
+        
+    # Parcours de tous les calques du groupe
+    print(layers)
+    for layer in layers:
+        print("------------for--------------")
+        print (layer)
+        # Vérification si le calque est de type texte
+        if pdb.gimp_item_is_text_layer(layer):
+            # Dupliquer le calque
+            duplicate = pdb.gimp_layer_copy(layer, True)
+		
+            # Ajouter le calque duplique au groupe "text-fr"
+            pdb.gimp_image_insert_layer(image, duplicate, group_text, 0)
+            
+            # Récupérer le contenu texte du calque dupliqué
+            text = pdb.gimp_text_layer_get_text(duplicate)
+            if text is None :
+                print ("----------text had markup-------------")
+                text = pdb.gimp_text_layer_get_markup(duplicate)
+                # quitter les markups avec une RegEx
+                text= re.sub(r"<.*?>", "", text)
+		
+            # essayer de traduir
+            try:
+                translated_text = translator.translate(text)
+            except Exception as e:
+                translated_text = "Erreur lors de la traduction : {}".format(e)
+                print("Erreur lors de la traduction : {}".format(e))
+
+            # vérifier s'il y a des caractères spéciaux html
+			# translated_text="J'aime les bananes &amp; les oranges &#128512;" #pour tester
+            if re.search(r"&[#\w]+;", translated_text):
+                print("-----------caractères html---------------")
+                # remplacer les caractères 
+                parser = HTMLDecodeParser()
+                parser.feed(translated_text)
+                translated_text = parser.result
+            # Mettre à jour le texte du calque dupliqué avec la traduction
+            pdb.gimp_text_layer_set_text(duplicate, translated_text)
+    # Actualisation de l'affichage de l'image
+    pdb.gimp_displays_flush()
+    pdb.gimp_message("fini")
+    # restore stuff
+    image.undo_group_end()
+
+register(
+    "python-fu-translate-text-layers",
+    "Translate text layers from French to English",
+    "Translate text layers from French to English",
+    "Your Name",
+    "Your Name",
+    "2023",
+    "<Image>/Filters/Language/Translate Text Layers",
+    "*",
+    [],
+    [],
+    translate_text_layers)
+
+main()
+
---
+++ translate/providers/libretranslatepy/__init__.py
@@ -0,0 +1,4 @@
+#from libretranslatepy.api import *
+# je remplace la ligne precedente pour voir si gimp arrete d'afficher un message derreur au demarrage
+#le message est ImportError: No module named libretranslatepy.api
+from api import *
